75be468ef6c652c1a1f11c4fec20c70bc393067b
MichaelCPP4 <michaelplay3452@gmail.com>
2022-12-14 23:06:03 +0300

    Добавьте файлы проекта.

---
+++ LibMas/TempData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMas
+{
+    public static class TempData
+    {
+        /// <summary>
+        /// Создаёт визуализацию даннных на DataGrid из передаваемого списков товаров, их затрат на производство и цен
+        /// </summary>
+        /// <param name="tovar">Коллекция наименований товаров</param>
+        /// <param name="productionCosts">коллекция затрат на производство товаров</param>
+        /// <param name="price">Коллекция цен на товары</param>
+        /// <returns>Представление таблицы</returns>
+        public static DataTable ToDataTable(List<string> tovar, List<double> productionCosts, List<double> price)
+        {
+            var tempData = new DataTable();
+
+            tempData.Columns.Add("Номер", typeof(uint));
+            tempData.Columns.Add("Товар", typeof(string));
+            tempData.Columns.Add("Цена на производство", typeof(double));
+            tempData.Columns.Add("Цена", typeof(double));
+
+            tempData.Columns[0].AutoIncrementSeed = 1;
+            tempData.Columns[0].AutoIncrement = true;
+
+            for (int i = 0; i < tovar.Count; i++)
+            {
+                DataRow row = tempData.NewRow();
+
+                row[1] = tovar[i];
+                row[2] = productionCosts[i];
+                row[3] = price[i];
+
+                tempData.Rows.Add(row);
+            }
+
+            return tempData;
+        }
+   
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibMas;
+
+namespace WPF_PR10_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dataGrid.ItemsSource = TempData.ToDataTable(tovar, productionCosts, prices).DefaultView;
+        }
+
+        List<string> tovar = new List<string>()
+        {
+            "Посудомойка",
+            "Стиральная машина",
+            "Лампа",
+            "Чайник",
+            "Миксер",
+            "Компьютер",
+            "Мониор"
+        };
+
+        List<double> prices = new List<double>()
+        {
+            44000.50,
+            35000.99,
+            2666.99,
+            1133.45,
+            3444.55,
+            66777.99,
+            21250.99
+
+        };
+        List<double> productionCosts = new List<double>()
+        {
+            32000.50,
+            31000.99,
+            1337.99,
+            800.45,
+            1488.55,
+            80228.99,
+            16250.99
+        };
+
+
+        private void ToFind_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < prices.Count; i++)
+            {
+                if (productionCosts[i] > prices[i])
+                {
+                    textBoxresult.Text = $"{i+1} - номер товара затраты производства которого превышают его цену";
+                    break;
+                }
+                else
+                {
+                    textBoxresult.Text = "Нет товаров затраты производства которого превышают его цену";
+                }
+            }
+        }
+    }
+}
